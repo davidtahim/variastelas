b53671f8a755f25ded346e7ba0325be91d6e0800
davidtahimalvesbrito <davidtahim@gmail.com>
2026-02-26 10:56:05 -0300

    Verificar o código para abrir telas na próxima aula
    Implement navigation methods in `MainActivity`
    
    - Declare `Button` variables for screen transitions.
    - Implement `abreTela2()` and `abreTela3()` methods to switch the content view to `tela2.xml` and `tela3.xml` respectively.
    - Configure click listeners within the transition methods to handle returning to the main screen via `voltaPrincipal()`.
    - Update imports to include `android.view.View` and `android.widget.*`.

--- app/src/main/java/com/example/variastelas/MainActivity.java
+++ app/src/main/java/com/example/variastelas/MainActivity.java
@@ -8,7 +8,40 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.*;
+
 public class MainActivity extends AppCompatActivity {
+
+    //Declarar os botões
+    Button bt2, bt3;
+
+    //Método para abrir a tela 2
+    public void abreTela2() {
+        setContentView(R.layout.tela2);
+    //Botao para a tela principal
+        Button vbtv2 = findViewById(R.id.bt2);
+        vbtv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                voltaPrincipal();
+            }
+        });
+        //Método para abrir a tela 3
+        public void abreTela3() {
+            setContentView(R.layout.tela3);
+            //Botao para a tela principal
+            Button vbtv3 = findViewById(R.id.bt3);
+            vbtv2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    voltaPrincipal();
+                }
+            });
+            {
+            }
+            }
+        }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
